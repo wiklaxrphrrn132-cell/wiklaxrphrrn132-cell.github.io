cb3651258b9b8fb904166cffc6ab3f81ef06cef4
wiklaxrphrrn132-cell <wiklaxrphrrn132@gmail.com>
2026-08-18 11:20:38 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
 document.addEventlistener('DOMContentLoaded',function () {
-  document.getElementByIy('year').innerHTML = new Date().getFullYear();
+  document.getElementById('year').innerHTML = new Date().getFullYear();
   console.log('Portfolio ของ Jakarin พร้อมแล้ว !);
               });
