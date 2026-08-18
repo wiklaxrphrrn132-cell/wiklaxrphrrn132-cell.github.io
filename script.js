b36d3d7b9f0cc47f8d5beef31d8efccb536cdaff
wiklaxrphrrn132-cell <wiklaxrphrrn132@gmail.com>
2026-08-18 11:46:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded',function () {
-  document.getElementById('year').innerHTML = new Date().getFullYear();
-  console.log('Portfolio ของ Oraphan พร้อมแล้ว !');
+document.addEventListener(‘DOMContentLoaded’,function () {
+    document.getElementById(‘year’).innerHTML = new Date().getFullYear();
+    console.log(‘Portfolio ของ Oraphan พร้อมแล้ว!’);
 });
