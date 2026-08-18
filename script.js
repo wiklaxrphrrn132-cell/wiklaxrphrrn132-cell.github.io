68563cf44a5abdeccee34fdb72c0f737177853ba
wiklaxrphrrn132-cell <wiklaxrphrrn132@gmail.com>
2026-08-18 11:40:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventlistener('DOMContentLoaded',function () {
   document.getElementById('year').innerHTML = new Date().getFullYear();
   console.log('Portfolio ของ Oraphan พร้อมแล้ว !');
-              });
+});
